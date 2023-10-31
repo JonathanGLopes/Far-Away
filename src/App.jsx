96821cfd66974758330208bce53e798e1e050c48
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-10-31 19:56:35 -0300

    adicionando key no componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { func } from "prop-types";
 import "./index.css";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "Charger", quantity: 3, packed: false },
+  { id: 3, description: "Charger", quantity: 3, packed: false },
 ];
 
 export default function App() {
@@ -35,7 +34,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
